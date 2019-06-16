fdac784d3b29d0bcae00d30a065a3c5cfeb3abad
rahulmistry33 <priya0307@gmail.com>
2019-06-16 17:16:44 +0530

    added flask

--- app.py
+++ app.py
@@ -1,81 +1,87 @@
 import requests
 import speech_recognition as sr 
-from os import path
+from os import path,chdir,getcwd
 from pprint import pprint
 
 
 
+class MomGenerator:
+    def recognizerWithMicrophone():
 
-def recognizerWithMicrophone():
+        r=sr.Recognizer()#creating instance of recognizer class
 
-    r=sr.Recognizer()#creating instance of recognizer class
+        #using microphone for source input
+        with sr.Microphone() as source:
+            print("Speak!")
+            audio = r.listen(source) #collecting the audio for recognizer
+            print("Over")
 
-    #using microphone for source input
-    with sr.Microphone() as source:
-        print("Speak!")
-        audio = r.listen(source) #collecting the audio for recognizer
-        print("Over")
+        #translate..
 
-    #translate..
+        try:
+            text = r.recognize_google(audio) #the text stores whatever google api returns 
+            print("you said,",text) #printing what google api returned
 
-    try:
-        text = r.recognize_google(audio) #the text stores whatever google api returns 
-        print("you said,",text) #printing what google api returned
-
-    except:
-        print("Could not recognize")
-        #exception if google api doesn't understand what you said
+        except:
+            print("Could not recognize")
+            #exception if google api doesn't understand what you said
 
 
-def recognizerWithAudioFile():
-    audio_file=path.join(path.dirname(path.realpath(__file__)),"test2.wav")
-    r=sr.Recognizer()
-    with sr.AudioFile(audio_file) as source:
-        audio=r.record(source)
-
-    try:
-        output=r.recognize_google(audio)#show all = true will show all possibilites of how google translates this audio to text
-        #print("Over")
-
-        #sending output to punctuator api using post request
-        url_punctuator="http://bark.phon.ioc.ee/punctuator"
-        data={'text':'{}'.format(output)}
-        response_from_punctuator=requests.request("POST",url_punctuator,data=data)
-        print("text returned from punctuator api:",response_from_punctuator.text)
-        punctuated_text=response_from_punctuator.text
+    def recognizerWithAudioFile(fname):
         
+        chdir(r"static\uploads")
+        audio_file=path.join(path.dirname(path.realpath(__file__)),"{}".format(fname))
+        r=sr.Recognizer()
+        with sr.AudioFile(audio_file) as source:
+            audio=r.record(source)
+
+        try:
+            output=r.recognize_google(audio)#show all = true will show all possibilites of how google translates this audio to text
+            #print("Over")
+
+            #sending output to punctuator api using post request
+            url_punctuator="http://bark.phon.ioc.ee/punctuator"
+            data={'text':'{}'.format(output)}
+            response_from_punctuator=requests.request("POST",url_punctuator,data=data)
+            #print("text returned from punctuator api:",response_from_punctuator.text)
+            punctuated_text=response_from_punctuator.text
+            
 
 
-        #sending the punctuated text to meaningcloud api using the api key
-        url = "https://api.meaningcloud.com/summarization-1.0"
+            #sending the punctuated text to meaningcloud api using the api key
+            url = "https://api.meaningcloud.com/summarization-1.0"
 
-        payload = "key=440a3e9fde785d6b5aa4bd1595052891&txt={}&url=&doc=&sentences=3".format(punctuated_text)
-        headers = {'content-type': 'application/x-www-form-urlencoded'}
-        #application/x-www-form-urlencoded
+            payload = "key=440a3e9fde785d6b5aa4bd1595052891&txt={}&url=&doc=&sentences=3".format(punctuated_text)
+            headers = {'content-type': 'application/x-www-form-urlencoded'}
+            #application/x-www-form-urlencoded
 
-        response = requests.request("POST", url, data=payload,headers=headers)
-        #headers=headers
+            response = requests.request("POST", url, data=payload,headers=headers)
+            #headers=headers
 
-   
-        #printing json object
-        print("text returned from meaning cloud api:",response.json()['summary'])
-        text=response.json()['summary']
+    
+            #printing json object
+           # print("text returned from meaning cloud api:",response.json()['summary'])
+            text=response.json()['summary']
 
-        
-        
-       # pprint("original output:",output)
+            
+            
+        # pprint("original output:",output)
 
-        #output also shows the accuracy of its conversion...check key confidence and transcript in output means the difference conversions of the audio file
-        
-    except:
-        print("Could not recognize")#exception if google api doesn't understand 
+            #output also shows the accuracy of its conversion...check key confidence and transcript in output means the difference conversions of the audio file
+            
+        except:
+            return "Could not recognize" #exception if google api doesn't understand 
+
+        return text
 
 
-choice=int(input("Enter a choice 1)Microphone 2)Audio File"))
-if choice==1:
-    recognizerWithMicrophone()
-else:
-    recognizerWithAudioFile()
+    #choice=int(input("Enter a choice 1)Microphone 2)Audio File"))
+    '''if choice==1:
+        recognizerWithMicrophone()'''
+    #else:
+    #commenting out other stuffs
+
+    #recognizerWithAudioFile()
 
 
 
---
+++ server.py
@@ -0,0 +1,56 @@
+from flask import Flask,render_template,url_for,redirect,request,flash
+from werkzeug import secure_filename
+from app import MomGenerator as m
+import os
+
+#creating instance of Flask class
+app=Flask(__name__)
+app.config['SECRET_KEY']='0501d344495cc373a2a73670ca42ae80'
+
+
+
+'''@app.route("/")
+def call():
+    val=m.recognizerWithAudioFile()
+    return "{}".format(val)'''
+
+app.config["UPLOAD_AUDIO"]=r"static\uploads"
+app.config["ALLOWED_EXTENSIONS"]=["wav"]
+
+def allowed_file(filename):
+    extension=filename.rsplit(".",1)[1]
+    if extension in app.config["ALLOWED_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+
+@app.route("/",methods=['GET','POST'])
+def generate():
+
+    name=""
+    text=""
+    if request.method=="POST":
+        if request.files:
+
+            audio=request.files["audio"]
+            if not allowed_file(audio.filename):
+                flash(f"Only filenames with wav extension is allowed","danger")
+                return render_template(request.url)
+            
+            audio.save(os.path.join(app.config["UPLOAD_AUDIO"],audio.filename))
+            name=audio.filename
+            print("audio saved")
+            #return redirect(request.url)
+        text=m.recognizerWithAudioFile(name)
+    
+    return render_template("generator.html",text=text)
+
+
+   
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
