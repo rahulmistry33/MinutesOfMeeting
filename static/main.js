0deda37f6597d069979d1f21bd495bfa21c6fad1
rahulmistry33 <priyarahul0307@gmail.com>
2019-07-22 00:48:12 +0530

    added points generation in textarea

--- static/main.js
+++ static/main.js
@@ -1,12 +1,42 @@
 $( function() {
     $( "#datepicker" ).datepicker();
   } );
+  $(document).ready(function(){
 
-  $(document).ready(function() {  
-    $("#add-agenda").on("click", function() {  
-        $("#agenda-box").append('<input class="form-control agendas" type="text" placeholder="Agenda "><br>');  
-    });  
-    $("#delete-agenda").on("click", function() {  
-        $("#agenda-box").children().last().remove();  
-    });  
-});  
+    var counter = 2;
+		
+    $("#addButton").click(function () {
+				
+	if(counter>10){
+            alert("Only 10 agendas allowed");
+            return false;
+	}   
+		
+	var newTextBoxDiv = $(document.createElement('div'))
+	     .attr("id", 'TextBoxDiv' + counter);
+                
+	newTextBoxDiv.after().html('<label>Agenda '+ counter + ' : </label>' +
+	      '<input type="text" name="textbox' + counter + 
+	      '" id="textbox' + counter + '" value=""  ><br>');
+            
+     newTextBoxDiv.appendTo("#TextBoxesGroup");
+     
+
+
+				
+	counter++;
+     });
+
+     $("#removeButton").click(function () {
+	if(counter==1){
+          alert("No more textbox to remove");
+          return false;
+       }   
+        
+	counter--;
+			
+        $("#TextBoxDiv" + counter).remove();
+			
+     });
+    });
+
